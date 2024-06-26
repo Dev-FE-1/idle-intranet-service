fdce6c8bfdbe6486c53271c77265b04428ca9994
James <dawn147@snu.ac.kr>
2024-06-26 18:09:11 +0900

    style: prettier 적용 및 불필요한 코드 삭제 (#36)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,15 @@
-import './Button.css'
+import './Button.css';
 
 export class Button {
-  constructor({type, size, content}) {
-		this.type = type || 'primary'; //primary, secondary, tertiary, ghost, text
+  constructor({ type, size, content }) {
+    this.type = type || 'primary'; //primary, secondary, tertiary, ghost, text
     this.size = size || 'default'; //small
-		this.content = content || 'contents';
+    this.content = content || 'contents';
   }
 
-  html(){
+  html() {
     return `
       <button class='button btn-${this.size} btn-${this.type}'>${this.content}</button>
-    `
+    `;
   }
 }
