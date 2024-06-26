840657446c27aef5199a83ce62700307c9938540
deun <devdeun@gmail.com>
2024-06-26 20:11:34 +0900

    feat: Title 컴포넌트 추가 (#53)
    - Title 컴포넌트 추가 후 각 페이지에 적용

---
+++ src/components/Title/Title.js
@@ -0,0 +1,23 @@
+import './Title.css';
+
+export default class Title {
+  constructor({ title, subtitle, description, contents, desktopOnly }) {
+    this.title = title;
+    this.subtitle = subtitle || '';
+    this.description = description || '';
+    this.contents = contents || '';
+    this.desktopOnly = desktopOnly || '';
+  }
+
+  html() {
+    return `
+      <div class="page-title-container${this.desktopOnly && ' desktop-only'}">
+        <div class="wrapper">
+            ${this.subtitle && `<div class="page-subtitle">${this.subtitle}</div>`}
+            <h1 class="page-title">${this.title}</h1>
+            ${this.description && `<p class="page-title-description">${this.description}</p>`}
+        </div>
+      </div>
+    `;
+  }
+}
--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -1,10 +1,19 @@
 import Main from '../../components/Main';
+import Title from '../../components/Title/Title';
+import { MENUS } from '../../utils/constants';
 import './Members.css';
 
 export default class MembersPage extends Main {
+  constructor() {
+    super();
+    this.Title = new Title({
+      title: MENUS.MEMBERS,
+    });
+  }
+
   render() {
     this.$container.innerHTML = `
-        <h1>구성원</h1>
+      ${this.Title.html()}
     `;
   }
 }
