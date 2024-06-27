8757996d79f35b0e7d06430d7dc1dbd939799271
Yang_Hae_Seok <dawn147@snu.ac.kr>
2024-06-27 12:02:53 +0900

    style: 클래스 명 변경 (#37)

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,14 +3,14 @@ import './Input.css';
 export default class Input {
   constructor({ placeholder, type, id, readOnly }) {
     this.placeholder = placeholder || '';
-    this.type = type || 'text'; //text, password, email, date....
-    this.id = id; //input id 설정(label 연결 용도)
-    this.readOnly = readOnly || 'default'; //readOnly
+    this.type = type || 'text'; // text, password, email, date....
+    this.id = id; // input id 설정 (label 연결 용도)
+    this.readOnly = readOnly ? 'read-only' : 'default'; //true, false
   }
 
   html() {
     return `
-        <input type=${this.type} class='input input-${this.readOnly}' id=${this.id} placeholder=${this.placeholder}>
-        `;
+      <input type=${this.type} class='input input-${this.readOnly}' id=${this.id} placeholder=${this.placeholder}>
+    `;
   }
 }
