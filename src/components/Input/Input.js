eaf0b1dab061bb96759f85ea98a657f3a624c187
James <dawn147@snu.ac.kr>
2024-06-27 11:23:14 +0900

    feat: input 컴포넌트 추가 (#37)

---
+++ src/components/Input/Input.js
@@ -0,0 +1,17 @@
+import './Input.css';
+
+export default class Input {
+  constructor({ placeholder, type, id, readOnly }) {
+    this.placeholder = placeholder || '';
+    this.type = type || 'text'; //text, password, email, date....
+    this.id = id; //input id 설정(label 연결 용도)
+    this.readOnly = readOnly || 'default'; //readOnly
+  }
+
+  html() {
+    return `
+        <input type=${this.type} class='input input-${this.readOnly}' id=${this.id} placeholder=${this.placeholder}>
+        `;
+  }
+}
+s
