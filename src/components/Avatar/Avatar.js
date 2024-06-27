c5654a47aab0c66822b7497ebd76ef4a7d854d8b
deun <devdeun@gmail.com>
2024-06-27 10:39:27 +0900

    style: Avatar 컴포넌트 Eslint 오류 수정

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -9,9 +9,9 @@ export default class Avatar {
 
   html() {
     return `
-			<div class='profile-img-container img-${this.size} img-bd-${this.border}'>
-				<img src=${this.url} alt="프로필 사진" class="profile-img img-${this.size}"/>
-			</div>
+      <div class='profile-img-container img-${this.size} img-bd-${this.border}'>
+        <img src=${this.url} alt="프로필 사진" class="profile-img img-${this.size}"/>
+      </div>
     `;
   }
 }
