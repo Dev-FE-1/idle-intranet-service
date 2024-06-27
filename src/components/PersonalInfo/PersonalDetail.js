b54a2fbe460a62974798c0c4caf7113159f2bf46
deun <devdeun@gmail.com>
2024-06-28 08:34:04 +0900

    feat: 프로필 페이지 퍼블리싱 (#48)
    - 인사정보, 개인정보, 고용정보, 학력/경력 추가
    - 관리자 / 본인 / 다른 직원일 때 각각 권한에 맞는 내용이 보이도록 함

---
+++ src/components/PersonalInfo/PersonalDetail.js
@@ -0,0 +1,33 @@
+export default class PersonalDetail {
+  constructor({ title, info }) {
+    this.title = title;
+    this.info = info;
+  }
+
+  html() {
+    return `
+      <li>
+        <div class="wrapper">
+          <h2 class="personal-detail-title">${this.title}</h2>
+          <ul class="personal-detail-list">
+            ${
+              this.info.length
+                ? this.info
+                    .map(
+                      (detail) => `
+                  <li>
+                    <h3 class="personal-detail-subtitle">${detail.subtitle}</h3>
+                    <span class="personal-detail-info">${detail.contents}</span>
+                  </li>
+                `,
+                    )
+                    .join('')
+                : ''
+            }
+          </ul>
+          ${!this.info.length ? '<p class="admin-owner-only-text">본인 혹은 관리자만 열람할 수 있습니다.</p>' : ''}
+        </div>
+      </li>
+    `;
+  }
+}
