6ce706d27a06f0b3fec7ad9a722cfcd726ab6b89
seoyoonyi <yiseoyoon@gmail.com>
2024-06-28 15:40:46 +0900

    setting: ESLint 규칙 업데이트 및 조정 (#60)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,36 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
   rules: {
+    // Prettier integration
+    'prettier/prettier': 'error',
+
+    // Styling rules
+    'arrow-body-style': 'off',
+    'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
     'import/prefer-default-export': 'off',
+
+    // Essential rules
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['__filename', '__dirname'], allowAfterThis: true },
+    ],
     'no-param-reassign': [
       'error',
       { props: true, ignorePropertyModificationsFor: ['element'] },
     ],
-    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'no-restricted-globals': [
+      'error',
+      {
+        name: 'history',
+        message: 'Use window.history instead of directly using history.',
+      },
+    ],
   },
+  plugins: ['prettier'],
 };
