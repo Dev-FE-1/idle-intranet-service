d3c69023b283c6d3a6db2d348335124e669d3c75
James <dawn147@snu.ac.kr>
2024-06-30 19:27:36 +0900

    design: 돋보기 아이콘 추가 (#47)

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -1,7 +1,12 @@
 import Main from '../../components/Main';
 import Title from '../../components/Title/Title';
+import Input from '../../components/Input/Input';
+import Table from '../../components/Table/Table';
 import { MENUS } from '../../utils/constants';
 import './Members.css';
+import Icon from '../../components/Icon/Icon';
+import { magnifyingGlass } from '../../utils/icons';
+import Pagination from '../../components/Pagination/Pagination';
 
 export default class MembersPage extends Main {
   constructor() {
@@ -9,11 +14,89 @@ export default class MembersPage extends Main {
     this.Title = new Title({
       title: MENUS.MEMBERS,
     });
+    this.magnifyGlass = new Icon({ svg: magnifyingGlass });
+    this.input = new Input({ placeholder: '이름을 입력하세요' });
+    this.table = new Table({
+      headers: ['이름', '직무', '조직', '이메일', '연락처'],
+      contents: [
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+        [
+          '김직원',
+          '프론테엔드 개발자',
+          '검색서비스개발팀',
+          'hello@gmail.com',
+          '02-123-4567',
+        ],
+      ],
+    });
+    this.pagination = new Pagination({ currentPage: 5, maxPage: 10 });
   }
 
   render() {
     this.$container.innerHTML = `
-      ${this.Title.html()}
+      <div class='members-container'>
+        <div class='members-mobile-only'>
+          ${this.Title.html()}
+          <span>총 <em>50</em> 명</span>
+        </div>
+        <div class='members-content-container'>
+          <div class='members-search-header'>
+            <span>총 <em>50</em> 명</span>
+            <div class='members-input-container'>
+              ${this.input.html()}
+              <button>
+                ${this.magnifyGlass.html()}
+              </button>
+            </div>
+          </div>
+          ${this.table.html()}
+        </div>
+        <div class='pagination-container'>
+          ${this.pagination.html()}
+        </div>
+      </div>
     `;
+    this.pagination.render()
   }
 }
--- src/utils/icons.js
+++ src/utils/icons.js
@@ -62,3 +62,10 @@ export const chevron_right = `
 export const chevrons_right = `
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-right"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg>
 `;
+
+export const magnifyingGlass = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+  <path d="M7.33333 12.6667C10.2789 12.6667 12.6667 10.2789 12.6667 7.33333C12.6667 4.38781 10.2789 2 7.33333 2C4.38781 2 2 4.38781 2 7.33333C2 10.2789 4.38781 12.6667 7.33333 12.6667Z" stroke="#8B95A1" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M14.0001 14.0001L11.1001 11.1001" stroke="#8B95A1" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`;
