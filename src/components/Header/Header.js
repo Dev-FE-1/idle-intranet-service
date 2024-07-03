0fbd15736245da66bdbfd3118f3ea7fbcabe170c
James <dawn147@snu.ac.kr>
2024-07-03 17:07:45 +0900

    feat: 헤더에 로그아웃 버튼 추가 (#50)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import AuthService from '../Auth/AuthService.js';
+import Button from '../Button/Button.js';
 import './Header.css';
 
 export default class Header {
@@ -5,6 +7,12 @@ export default class Header {
     this.$container = document.querySelector(container);
     this.profileImage = user && user.profileImage;
     this.userName = user && user.name;
+    this.Button = new Button({
+      variant: 'tertiary',
+      content: '로그아웃',
+      size: 'small'
+    });
+    this.handleLogout = new AuthService().logout;
     this.render();
   }
 
@@ -18,7 +26,13 @@ export default class Header {
             alt="${this.userName || '김직원'}"
           />
         </div>
+        <div class ='logout-btn'>
+          ${this.Button.html()}
+        </div>
       </div>
     `;
+    document
+      .querySelector('.logout-btn button')
+      .addEventListener('click', this.handleLogout);
   }
 }
