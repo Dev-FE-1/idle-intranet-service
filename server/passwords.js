794dde18b8f0889ea38ce5673c74cd54038e468d
James <dawn147@snu.ac.kr>
2024-07-03 17:04:13 +0900

    feat: 비밀번호 검증 코드 (#50)

--- server/passwords.js
+++ server/passwords.js
@@ -5,3 +5,8 @@ export const encryptPassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   return hashedPassword;
 };
+
+export const verifyPassword = async (password, hashedPassword) => {
+  const match = await bcrypt.compare(password, hashedPassword);
+  return match;
+};
