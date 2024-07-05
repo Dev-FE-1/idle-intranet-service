2342ad7478e616527f16ac68625197c4235201cd
deun <devdeun@gmail.com>
2024-07-05 19:44:19 +0900

    feat: Container 컴포넌트에서 store 삭제
    - 필요한 컴포넌트에서 직접 import해서 사용하는 방식으로 변경

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,5 @@
-import { storeInstance } from './Store.js';
-
 export default class Container {
   constructor(element) {
     this.$container = document.querySelector(element);
-    this.store = storeInstance;
   }
 }
