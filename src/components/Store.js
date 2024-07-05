d32b293afe064c3b822ec1026c293a4ad0101c83
deun <devdeun@gmail.com>
2024-07-05 20:38:02 +0900

    feat: 프로필 페이지의 프로필 정보 실제 데이터로 변경 (#104)

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,16 @@
+import { fetchUser } from '../fetchData.js';
+
 class Store {
-  constructor() {}
+  constructor() {
+    this.user = null;
+  }
+
+  async getUser() {
+    if (this.user) return this.user;
+
+    const user = await fetchUser();
+    return user;
+  }
 }
 
 export const storeInstance = new Store();
