944767d0d5da0f97c7f8dc15297146faf7d81575
deun <devdeun@gmail.com>
2024-07-05 21:52:48 +0900

    feat: 404 페이지 마크업 (#103)

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,12 +1,17 @@
 import Main from '../../components/Main.js';
+import notFoundImage from '../../../public/images/404.png';
 import './PageNotFound.css';
 
 export default class PageNotFound extends Main {
   render() {
-    this.$container.innerHTML = `
-      <h1>404</h1>
-      <p>존재하지 않는 페이지입니다.</p>
-      <a href="/">처음으로 돌아가기</a>
+    this.$container.innerHTML = /* HTML */ `
+      <div class="not-found-container wrapper">
+        <img class="not-found-image" src=${notFoundImage} alt="404" />
+        <h1 class="not-found-title">페이지를 찾을 수 없습니다</h1>
+        <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+        <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+        <a class="home-link" href="/">홈으로 이동</a>
+      </div>
     `;
   }
 }
