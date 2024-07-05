213547053a8b8610fe41f1122d84e7143fe00b41
deun <devdeun@gmail.com>
2024-07-05 22:02:18 +0900

    design: 404 페이지 스타일 수정 (#103)

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -8,12 +8,14 @@ export default class PageNotFound {
 
   render() {
     this.$container.innerHTML = /* HTML */ `
-      <div class="not-found-container wrapper">
-        <img class="not-found-image" src=${notFoundImage} alt="404" />
-        <h1 class="not-found-title">페이지를 찾을 수 없습니다</h1>
-        <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
-        <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
-        <a class="home-link" href="/">홈으로 이동</a>
+      <div class="not-found-container">
+        <div class="wrapper">
+          <img class="not-found-image" src=${notFoundImage} alt="404" />
+          <h1 class="not-found-title">페이지를 찾을 수 없습니다</h1>
+          <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+          <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+          <a class="home-link" href="/">홈으로 이동</a>
+        </div>
       </div>
     `;
   }
