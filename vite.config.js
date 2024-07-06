691b2e167d5ce3afb2ba3ea8244ca5e3ff7b18a3
deun <devdeun@gmail.com>
2024-07-07 00:29:39 +0900

    feat: github pages 404 페이지 추가 (#112)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/idle-intranet-service/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        404: path.resolve(__dirname, '404.html'),
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
