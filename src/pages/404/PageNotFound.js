ec75d541a3f1059956380544dabd32dc0645399f
deun <devdeun@gmail.com>
2024-07-06 15:22:39 +0900

    feat: 404 페이지의 a 태그를 Button 컴포넌트로 변경 (#110)

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,11 +1,12 @@
-
 import Container from '../../components/Container.js';
 import notFoundImage from '../../../public/images/404.png';
 import './PageNotFound.css';
+import Button from '../../components/Button/Button.js';
 
 export default class PageNotFound extends Container {
   constructor() {
     super('#app');
+    this.homeButton = new Button({ content: '홈으로 이동' });
   }
 
   render() {
@@ -16,7 +17,7 @@ export default class PageNotFound extends Container {
           <h1 class="not-found-title">페이지를 찾을 수 없습니다</h1>
           <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
           <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
-          <a class="home-link" href="/">홈으로 이동</a>
+          ${this.homeButton.html()}
         </div>
       </div>
     `;
