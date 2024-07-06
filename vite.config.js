1f249bd372256b354d1e56ba3fa6bff1bd2d7181
deun <devdeun@gmail.com>
2024-07-06 23:05:00 +0900

    fix: vite.config.js 수정 (#112)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_SERVER_URL || 'http://localhost:8080',
+        target: process.env.VITE_SERVER_URL,
         changeOrigin: true,
         secure: false,
       },
