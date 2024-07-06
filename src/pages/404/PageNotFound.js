a71dc4161082a124132106169f9774235f578137
deun <devdeun@gmail.com>
2024-07-06 23:12:46 +0900

    fix: 404 페이지 경로 오류 수정 (#112)

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,7 +1,7 @@
-
 import Container from '../../components/Container.js';
 import notFoundImage from '../../../public/images/404.png';
 import './PageNotFound.css';
+import { PATH } from '../../utils/constants.js';
 
 export default class PageNotFound extends Container {
   constructor() {
@@ -16,7 +16,7 @@ export default class PageNotFound extends Container {
           <h1 class="not-found-title">페이지를 찾을 수 없습니다</h1>
           <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
           <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
-          <a class="home-link" href="/">홈으로 이동</a>
+          <a class="home-link" href="${PATH.HOME}">홈으로 이동</a>
         </div>
       </div>
     `;
