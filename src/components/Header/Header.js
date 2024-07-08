2ee77270089cdb2acc687cedf2793970834b5572
Yang_Hae_Seok <dawn147@snu.ac.kr>
2024-07-08 10:48:02 +0900

    Header.js solve conflict (#83)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import { logout } from '../API/AuthService.js';
+import Avatar from '../Avatar/Avatar.js';
 import Button from '../Button/Button.js';
 import Container from '../Container.js';
+import { storeInstance } from '../Store.js';
 import './Header.css';
 
 export default class Header extends Container {
   constructor() {
     super('.header-container');
-    // this.profileImage = user && user.profileImage;
-    // this.userName = user && user.name;
+    this.store = storeInstance;
     this.Button = new Button({
       variant: 'tertiary',
       content: '로그아웃',
@@ -15,16 +16,22 @@ export default class Header extends Container {
     });
   }
 
+  async renderAvatar() {
+    if (!this.user) {
+      this.user = await this.store.getUser();
+      this.Avatar = new Avatar({
+        url: this.user.profileImage,
+      });
+    }
+
+    const $avatarContainer = document.querySelector('header .avatar-container');
+    $avatarContainer.innerHTML = this.Avatar.html();
+  }
+
   render() {
     this.$container.innerHTML = `
       <div class="wrapper">
-        <div class="header-profile">
-          <img
-            class="header-profile-image"
-            src="${this.profileImage || 'https://api.dicebear.com/9.x/lorelei/svg?seed=Max&eyes=variant09'}"
-            alt="${this.userName || '김직원'}"
-          />
-        </div>
+        <div class="avatar-container"></div>
         <div class ='logout-btn-wrapper'>
           ${this.Button.html()}
         </div>
@@ -33,5 +40,7 @@ export default class Header extends Container {
     document
       .querySelector('.logout-btn-wrapper button')
       .addEventListener('click', logout);
+
+    this.renderAvatar();
   }
 }
