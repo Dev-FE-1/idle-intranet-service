3d15bd519c745ad32af97b0643415742d4d092c0
deun <devdeun@gmail.com>
2024-07-10 08:38:14 +0900

    feat: 랜덤 프로필 이미지 생성 옵션 추가 (#85)

--- src/utils/profileImage.js
+++ src/utils/profileImage.js
@@ -21,13 +21,27 @@ const SEEDS = [
   'Kiki',
 ];
 const EYES_VARIANTS = 24;
+const HAIR_VARIANTS = 48;
+const EYEBROWS_VARIANTS = 13;
+const GLASSES_VARIANTS = 5;
 const PROFILE_IMAGE_BASE_URL = 'https://api.dicebear.com/9.x/lorelei/svg';
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
 
 function generateRandomProfile() {
   const randomSeed = SEEDS[Math.floor(Math.random() * SEEDS.length)];
-  const randomEyes = `${String(Math.floor(Math.random() * EYES_VARIANTS) + 1).padStart(2, '0')}`;
+  const randomEyes = `${String(getRandomNumber(EYES_VARIANTS)).padStart(2, '0')}`;
+  const randomHair = `${String(getRandomNumber(HAIR_VARIANTS)).padStart(2, '0')}`;
+  const randomEyebrows = `${String(getRandomNumber(EYEBROWS_VARIANTS)).padStart(2, '0')}`;
 
-  return `${PROFILE_IMAGE_BASE_URL}?seed=${randomSeed}&eyes=variant${randomEyes}`;
+  let randomGlasses = '';
+  if (Math.random() < 0.3) {
+    randomGlasses = `&glasses=variant${String(getRandomNumber(GLASSES_VARIANTS)).padStart(2, '0')}`;
+  }
+
+  return `${PROFILE_IMAGE_BASE_URL}?seed=${randomSeed}&eyes=variant${randomEyes}&hair=variant${randomHair}&eyebrows=variant${randomEyebrows}${randomGlasses}`;
 }
 
 export { generateRandomProfile };
