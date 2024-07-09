3d4247d326e4050f4b2783a9d5a0fe356c10a88b
deun <devdeun@gmail.com>
2024-07-09 11:27:42 +0900

    feat: 기본 이미지 설정 버튼 기능 추가 (#85)

--- src/components/PersonalInfo/EditProfileForm.js
+++ src/components/PersonalInfo/EditProfileForm.js
@@ -3,6 +3,7 @@ import Icon from '../Icon/Icon.js';
 import Input from '../Input/Input.js';
 import { camera } from '../../utils/icons.js';
 import './EditProfileForm.css';
+import { generateRandomProfile } from '../../utils/profileImage.js';
 
 export default class EditProfileForm {
   constructor({ member }) {
@@ -24,6 +25,21 @@ export default class EditProfileForm {
     });
   }
 
+  setRandomImage() {
+    this.newImage = generateRandomProfile();
+    this.$image = document.querySelector('.edit-profile-form .profile-image');
+    this.$image.src = this.newImage;
+  }
+
+  setEventListeners() {
+    this.$randomImageButton = document.querySelector(
+      '.edit-profile-form .random-profile-image-button',
+    );
+    this.$randomImageButton.addEventListener('click', () =>
+      this.setRandomImage(),
+    );
+  }
+
   renderInputs() {
     const $phoneNumberInput = document.querySelector(
       'input#phone_number_input',
@@ -36,17 +52,22 @@ export default class EditProfileForm {
 
   render() {
     this.renderInputs();
+    this.setEventListeners();
   }
 
   html() {
     return /* HTML */ `<form class="edit-profile-form">
       <div class="edit-profile-image-container">
         <div class="profile-image-container">
-          <img src="${this.member.profileImage}" alt="${this.member.name}" />
+          <img
+            src="${this.member.profileImage}"
+            alt="${this.member.name}"
+            class="profile-image"
+          />
         </div>
         <div class="edit-image-button-container">${this.Button.html()}</div>
       </div>
-      <button class="delete-profile-image-button">이미지 삭제</button>
+      <button class="random-profile-image-button">기본 이미지 설정</button>
       <div class="input-container">
         <label for="phone_number_input">전화번호</label>
         ${this.phoneNumberInput.html()}
