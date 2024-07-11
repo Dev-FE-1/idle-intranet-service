5ef2b4a12f2b321cb663d41edf99372d2854b801
seoyoonyi <yiseoyoon@gmail.com>
2024-07-11 18:55:37 +0900

    feat: vacation 관련 배열 추가  (#49)

---
+++ src/utils/vacation.js
@@ -0,0 +1,75 @@
+export const vacationArray = [
+  {
+    icon: '🌴',
+    type: '연차',
+    days: '17일',
+  },
+  {
+    icon: '💼',
+    type: '반차',
+    days: '3시간',
+  },
+  {
+    icon: '🤰🏻',
+    type: '출산 휴가',
+    days: '90일',
+  },
+  {
+    icon: '🫃🏻',
+    type: '배우자 출산 휴가',
+    days: '30일',
+  },
+  {
+    icon: '🎀',
+    type: '여성 휴가',
+    days: '1일',
+  },
+  {
+    icon: '👥',
+    type: '가족 돌봄 휴가',
+    days: '1일',
+  },
+  {
+    icon: '👤',
+    type: '기타 휴가',
+    days: '1일',
+  },
+];
+
+export const vacationTypes = [
+  '전체 휴가',
+  '연차',
+  '반차',
+  '출산 휴가',
+  '배우자 출산 휴가',
+  '여성 휴가',
+  '가족 돌봄 휴가',
+  '기타휴가',
+];
+
+export const vacationDaysOptions = [
+  '0일',
+  '1일',
+  '2일',
+  '3일',
+  '4일',
+  '5일',
+  '6일',
+  '7일',
+  '8일',
+];
+export const availableStartTimes = [
+  '00:00',
+  '09:00',
+  '10:00',
+  '11:00',
+  '12:00',
+  '13:00',
+  '14:00',
+  '15:00',
+  '16:00',
+  '17:00',
+  '18:00',
+];
+
+export const hourIncrements = ['0시간', '1시간', '2시간', '3시간', '4시간'];
